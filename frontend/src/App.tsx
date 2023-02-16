accfe872c3b63c57b7b87ec3af63a5c0b17bbbbf
Luiz Cornelli <luizcornelli.dev@gmail.com>
2023-02-16 20:45:06 -0300

    Add prettier code formatter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,7 @@ import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
-  return (
-    <h1>
-      Hellow DSCatalog!
-    </h1>
-  );
+  return <h1>Hellow DSCatalog!</h1>;
 }
 
 export default App;
